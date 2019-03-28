ee9a27e0f0e7ce5fcc3ef0779a7d8fefe4ffd11b
CyrilGuilloud@pcsht <cyril.guilloud@esrf.fr>
2019-03-28 14:04:56 +0100

    adapt setup.py to make package

---
+++ README.md
@@ -0,0 +1,24 @@
+# Xcalibu
+
+
+author: Cyril Guilloud ESRF BCU 2013-2019
+
+
+Xcalibu is a python library to manage calibrations tables or polynomia.
+
+xcalibu.py : python library
+Xcalibuds.py : PyTango device server
+
+
+## installation
+
+
+## usage
+
+to plot: `./xcalibu.py -p`
+
+to debug:`./xcalibu.py -d10`
+
+plot a file:`./xcalibu.py -p examples/xcalibu_calib_poly.calib`
+
+
--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
-from distutils.core import setup
+import setuptools
 
-setup(name="calibu", version="0.1",
-      description="ESRF Calibration library",
-      author="C.Guilloud (ESRF)",
-      py_modules=['xcalibu', 'timedisplay']
-      )
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="calibu",
+    version="0.9.0",
+    description="Calibration library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.esrf.fr/bliss/xcalibu",
+    author="Cyril Guilloud (ESRF-BCU)",
+    author_email="prenom.name@truc.fr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
 
